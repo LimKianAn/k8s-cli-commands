08c94ba2e70377eddd88018da242cb739fd88256
Chien-An Lin <chien-an.lin@iqmine.com>
2020-05-28 08:38:38 +0200

    Add labels to metadata (spec.template.metadata.labels doesn't propogate)

--- deployment/commands.sh
+++ deployment/commands.sh
@@ -1,1 +1,5 @@
 curl --output nginx.deployment.yml https://raw.githubusercontent.com/LimKianAn/k8s-cli-commands/master/deployment/nginx.deployment.yml
+k create --filename nginx.deployment.yml --save-config
+k get all
+k describe deployment nginx-again
+k get deployments --show-labels # LABELS not working
