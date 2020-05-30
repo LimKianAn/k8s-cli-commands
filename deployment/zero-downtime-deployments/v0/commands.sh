a272614cd41291edf6a86cbf45ff9aae666e2caa
Chien-An Lin <chien-an.lin@iqmine.com>
2020-05-30 12:21:43 +0200

    Add commands

---
+++ deployment/zero-downtime-deployments/v0/commands.sh
@@ -0,0 +1,15 @@
+# Remove containers
+docker ps -all
+docker container rm <container ID>
+
+# Remove images
+docker image ls
+docker image rm <image ID>
+
+# Go to where Dockerfile is
+
+# Build image
+docker image build . --tag node-server:0.0
+
+# Run container
+docker container run -d -p 8080:8080 node-server:0.0
